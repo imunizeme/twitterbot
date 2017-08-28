812d8b54f875ac57e62c69107d36c14b73baf368
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-08-28 10:24:47 -0300

    use data instead notification

--- gcm.go
+++ gcm.go
@@ -8,17 +8,14 @@ import (
 
 func send(text string) error {
 	ids := make([]string, 0)
-	n := &gcm.Notification{
-		Title:       "Notícia",
-		Body:        text,
-		Icon:        "ic_imunizeme_launcher",
-		ClickAction: "WebViewActivity",
+	data := map[string]string{
+		"text": text,
 	}
 	sender := gcm.NewSender(config.Get.Bot.MessageToken)
 	err := postgres.Query("SELECT * FROM notification_clients").Scan(&ids)
 	if err != nil {
 		return err
 	}
-	_, err = sender.SendMulticastNoRetry(&gcm.Message{Notification: n}, ids)
+	_, err = sender.SendMulticastNoRetry(&gcm.Message{Data: data}, ids)
 	return err
 }
